79acbde91e197707806097ef6674bf4ace120942
DM Brar <dmvbrar@gmail.com>
2024-03-31 17:15:31 -0400

    added list layout on main activity

--- app/src/main/java/com/example/donnamariebrar_comp304sec004_lab4/MainActivity.kt
+++ app/src/main/java/com/example/donnamariebrar_comp304sec004_lab4/MainActivity.kt
@@ -3,13 +3,41 @@ package com.example.donnamariebrar_comp304sec004_lab4
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.ExperimentalLayoutApi
+import androidx.compose.foundation.layout.FlowColumn
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.itemsIndexed
+import androidx.compose.material3.DropdownMenu
+import androidx.compose.material3.DropdownMenuItem
+import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
+import androidx.compose.material3.TextField
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.text.font.FontFamily
+import androidx.compose.ui.text.font.FontStyle
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.text.style.TextDecoration
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.example.donnamariebrar_comp304sec004_lab4.ui.theme.DonnaMarieBrar_COMP304Sec004_Lab4Theme
 
 class MainActivity : ComponentActivity() {
@@ -22,25 +50,60 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    Greeting("Android")
+                    Layout()
                 }
             }
         }
     }
 }
 
+//Layout
+@OptIn(ExperimentalLayoutApi::class)
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
+fun Layout() {
+    FlowColumn(modifier = Modifier.padding(24.dp),
+                verticalArrangement = Arrangement.spacedBy(24.dp))
+    {
+        Text(
+            text = "Please select a program",
+            fontSize = 28.sp,
+            fontWeight = FontWeight.Bold,
+            textAlign = TextAlign.Center,
+            fontFamily = FontFamily.SansSerif,
+            letterSpacing = 1.sp,
+            lineHeight = 36.sp,
+        )
+        ProgList()
+    }
+}
+
+//List
+@Composable
+fun ProgList() {
+    val context = LocalContext.current
+    val programs = context.resources.getStringArray(R.array.swProgs).toList()
+    var selectedItemIndex by remember { mutableStateOf(-1) }
+
+    LazyColumn(modifier = Modifier.fillMaxWidth(),
+                verticalArrangement = Arrangement.spacedBy(8.dp))
+    {
+        itemsIndexed(programs) { index, program ->
+            Text(
+                text = program,
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .clickable { selectedItemIndex = index }
+                    .background(if (index == selectedItemIndex) Color.LightGray else Color.White)
+                    .padding(16.dp)
+            )
+        }
+    }
 }
 
 @Preview(showBackground = true)
 @Composable
-fun GreetingPreview() {
+fun LayoutPreview() {
     DonnaMarieBrar_COMP304Sec004_Lab4Theme {
-        Greeting("Android")
+        Layout()
     }
 }
--- app/src/main/java/com/example/donnamariebrar_comp304sec004_lab4/ui/theme/Color.kt
+++ app/src/main/java/com/example/donnamariebrar_comp304sec004_lab4/ui/theme/Color.kt
@@ -2,8 +2,8 @@ package com.example.donnamariebrar_comp304sec004_lab4.ui.theme
 
 import androidx.compose.ui.graphics.Color
 
-val Purple80 = Color(0xFFD0BCFF)
-val PurpleGrey80 = Color(0xFFCCC2DC)
+val Purple80 = Color(0xFFE8E0FA)
+val PurpleGrey80 = Color(0xFFEEE8F8)
 val Pink80 = Color(0xFFEFB8C8)
 
 val Purple40 = Color(0xFF6650a4)
